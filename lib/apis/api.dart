7584d82c75d86c72c98471bc6022558a247186a9
이원희 <story3798@naver.com>
2023-06-11 17:28:01 +0900

    노선명 검색결과 UI 구성

--- lib/apis/api.dart
+++ lib/apis/api.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 
 busLineSeach(String number) async { // 버스 노선 조회 API
   var data = {};
+  var datas = [];
   var result = await http.get(
       Uri.parse('http://openapi.gbis.go.kr/ws/rest/busrouteservice?serviceKey=1234567890&keyword=${number}'));
   if (result.statusCode == 200) { // API 응답 코드 (정상처리)
@@ -11,9 +12,12 @@ busLineSeach(String number) async { // 버스 노선 조회 API
     var Xml2JsonData = Xml2Json()..parse(getXmlData); // XML에서 JSON 형식으로 데이터 변환
     var jsonData = Xml2JsonData.toParker();
     data = jsonDecode(jsonData); //JSON 형식으로 디코딩
-    var a = data['response']['msgBody']; // 필터링
-    return (a['busRouteList']).toString(); // 값 반환
-  } else {
-    return "정보 불러오기 실패";
+    data = data['response']['msgBody']; // 필터링
+    try{
+      datas = data['busRouteList'];
+    }catch (e){
+      datas.add(data['busRouteList']);
+    }
+    return datas; // 값 반환
   }
 }
--- lib/view/search.dart
+++ lib/view/search.dart
@@ -13,6 +13,7 @@ class _SearchState extends State<Search> {
   var data;
   var datas;
   var input = "";
+  var colors;
 
   getData(String c) async {
     datas = await busLineSeach(c);
@@ -21,9 +22,35 @@ class _SearchState extends State<Search> {
     });
   }
 
+  getColor(String color){
+    if(color == "직행좌석형시내버스"){
+      colors = Colors.redAccent;
+    }else if(color == "좌석형시내버스"){
+      colors = Colors.blue;
+    }else if(color == "일반형시내버스"){
+      colors = Colors.green;
+    }else if(color == "광역급행형시내버스"){
+      colors = Colors.red;
+    }else if(color == "마을버스"){
+      colors = Colors.yellow;
+    }else{
+      colors = Colors.grey;
+    }
+    return colors;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      data = [{"routeName":"N/A","routeTypeName":"N/A","regionName":"N/A","routeId":"N/A"}];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(100.0),
         child: Hero(
@@ -60,33 +87,133 @@ class _SearchState extends State<Search> {
           ),
         ),
       ),
-      body: ListView(
+      body: Column(
         children: [
-          TextButton(
-            onPressed: (){
-              print("눌림");
-            },
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-            ),
-            child: Container(
-              height: 700,
-              width: double.infinity,
-              margin:EdgeInsets.fromLTRB(10,0, 10, 0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.vertical(bottom: Radius.circular(15),top: Radius.circular(15)),
-                color: Colors.teal,
-              ),
-              child: Center(
-                child: Text(
-                  data.toString(),
-                  style: TextStyle(color: Colors.white,fontSize: 10),
-                ),
-              ),
+          Container(
+            padding: EdgeInsets.only(left: 20,top: 5,bottom: 5),
+            alignment: Alignment.centerLeft,
+            child: Text("노선 검색 결과",style: TextStyle(fontSize: 20),),
+          ),
+          Container(
+            width: double.infinity,
+            height: 350,
+            child: ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (c,i){
+                  return TextButton(
+                    onPressed: (){
+                      print(data[i]['routeId']);
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                    ),
+                    child: Container(
+                        height: 70,
+                        width: double.infinity,
+                        margin:EdgeInsets.fromLTRB(10,0, 10, 0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.vertical(bottom: Radius.circular(15),top: Radius.circular(15)),
+                          color: getColor(data[i]['routeTypeName'].toString()),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 20),
+                              child: Image.network("https://media.discordapp.net/attachments/905797523363483659/1116366006969962626/green_bus.png?width=432&height=432"),
+                            ),
+                            Container(
+                                width: 280,
+                                child:Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.only(top: 9,right: 10),
+                                      child: Text(
+                                        "${data[i]['routeName'].toString()}번",
+                                        style: TextStyle(color: Colors.white,fontSize: 30),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(right: 10),
+                                      child: Text(
+                                        "${data[i]['regionName'].toString()}버스",
+                                        style: TextStyle(color: Colors.white,fontSize: 15),
+                                      ),
+                                    )
+                                  ],
+                                )
+                            ),
+                          ],
+                        )
+                    ),
+                  );
+                }
             ),
           ),
-        ]
-      ),
+          Container(
+            padding: EdgeInsets.only(left: 20,top: 5,bottom: 5),
+            alignment: Alignment.centerLeft,
+            child: Text("정류장 검색 결과",style: TextStyle(fontSize: 20),),
+          ),
+          Container(
+            width: double.infinity,
+            height: 300,
+            child: ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (c,i){
+                  return TextButton(
+                    onPressed: (){
+                      print(data[i]['routeId']);
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                    ),
+                    child: Container(
+                        height: 60,
+                        width: double.infinity,
+                        margin:EdgeInsets.fromLTRB(10,0, 10, 0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.vertical(bottom: Radius.circular(15),top: Radius.circular(15)),
+                          color: Colors.blueGrey,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 20,right: 20),
+                              child: Image.network("https://media.discordapp.net/attachments/905797523363483659/1116527200972308560/bus-stop.png?width=432&height=432",scale: 10,),
+                            ),
+                            Container(
+                                child:Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.only(top: 10,right: 10),
+                                      child: Text(
+                                        "정류장명 검색 결과 표시 예정",
+                                        style: TextStyle(color: Colors.white,fontSize: 20),
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: EdgeInsets.only(right: 10),
+                                      child: Text(
+                                        "정류장 위치 표시 예정",
+                                        style: TextStyle(color: Colors.white,fontSize: 15),
+                                      ),
+                                    )
+                                  ],
+                                )
+                            ),
+                          ],
+                        )
+                    ),
+                  );
+                }
+            ),
+          ),
+        ],
+      )
     );
   }
 }
